741f25f0a092a440a86825d5c37f752bd6b846df
olakin <nikolamiric66@gmail.com>
2025-01-31 23:57:36 +0100

    Shadow

--- resources/views/pocetna.blade.php
+++ resources/views/pocetna.blade.php
@@ -7,14 +7,14 @@
     <title>Document</title>
     @vite('resources/css/app.css')
 </head>
-<body class="bg-neutral-400 grid grid-cols-1 grid-rows-4 justify-items-center"> 
-    <div class="w-screen h-40 flex justify-center m-0 p-6 row-span-1 bg-neutral-400 border-4 border-neutral-500">
+<body class="bg-neutral-100 grid grid-cols-1 grid-rows-4 justify-items-center"> 
+    <div class="w-screen h-40 flex justify-center m-0 p-6 row-span-1  border-4 border-neutral-500">
         <img class="w-30 h-30 mr-4" src="/slike/polica.png" alt="Polica s knjigama"> 
         <div class="flex items-center" ><h1 class="text-7xl font-sans mb-0 ">Biblioteka</h1></div>
     </div>
-    <div class="w-screen flex justify-center m-0 p-6 row-span-2 bg-neutral-400 border-4 border-neutral-500"><p>Tekst</p></div>
-    <div class="w-screen h-40 flex justify-center m-0 p-6 row-span-1 bg-neutral-400 border-4 border-neutral-500">
-        <p>Made by me <button href="/">Back to top</button></p>
+    <div class="w-screen flex justify-center m-0 p-6 row-span-2 bg-neutral-400 border-4 border-neutral-500"><p>Teasdadkst</p></div>
+    <div class="w-screen h-40 flex justify-between content-center m-0 p-6 row-span-1 bg-neutral-400 border-4 border-neutral-500">
+        <p class="self-center">Made by me </p><button href="/">Back to top</button>
     </div>
 </body> 
 </html>
