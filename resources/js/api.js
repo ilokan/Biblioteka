25fdcb0f6a217adff440a17207ede86471e07fd9
olakin <nikolamiric66@gmail.com>
2025-02-15 22:10:42 +0100

    Sitne prepravke

--- resources/js/api.js
+++ resources/js/api.js
@@ -3,7 +3,7 @@ window.$ = jQuery;
 
 
 var dugme = document.querySelector("#dugme");
-var brojac=1;
+var brojac=0;
 
 dugme.addEventListener("click",brojanje =>{
     brojac +=1;
@@ -15,10 +15,12 @@ if(brojac>5){
     dugme.removeEventListener("click",dajKnjige);
     dugme.removeEventListener("click",brojanje);
 }
-var naslov='<div id="naslov'+brojac+'"';
-var autor='<div id="autor'+brojac+'"';
-var slika='<div id="slika'+brojac+'"';
+var naslov="";
+var autor="";
+var slika="";
+var broj="";
 
+// Funkcija koja ubacuje podatke o knjigama iz Open Library
 async function dajKnjige(){
     try{
         const pretraga = document.querySelector("#unos").value.trim();
@@ -29,9 +31,13 @@ async function dajKnjige(){
         autor=data.docs[0].author_name;
         document.querySelector("#autor"+brojac).innerHTML=autor;
         slika= document.querySelector("#slika"+brojac);
-        slika.src = 'http://covers.openlibrary.org/b/id/'+data.docs[0].cover_i+'-M.jpg'
-        slika.style.display = "block"
+        slika.src = 'http://covers.openlibrary.org/b/id/'+data.docs[0].cover_i+'-M.jpg';
+        slika.style.display = "block";
         slika.style.margin = "0 auto";
+        document.querySelector("#x"+brojac).innerHTML="&#9747;";
+        broj=document.querySelector("#broj"+brojac);
+        console.log("razmak");
+        broj.style.display="none";
   
       
     }
@@ -39,7 +45,7 @@ async function dajKnjige(){
         console.error(error);
     }
 }
-
+// Uklanjanje X
 document.addEventListener("click", (e)=>{
     let ukloni = e.target.id;
     if(ukloni === "x1" || ukloni === "x2" || ukloni === "x3" || ukloni === "x4" || ukloni === "x5"){
@@ -47,6 +53,7 @@ document.addEventListener("click", (e)=>{
         $(e.target.closest("div")).find("h1").html("");
         $(e.target.closest("div")).find("p").html("");
         $(e.target.closest("div")).find("img").css("display","none");
+        $(e.target.closest("div")).find("button").css("display","none");
 
     }
 
--- resources/views/pocetna.blade.php
+++ resources/views/pocetna.blade.php
@@ -7,46 +7,68 @@
     @vite('resources/css/app.css')
     <title>Biblioteka</title>    
 </head>
-<body class="grid grid-cols-1 grid-rows-4 h-vh bg-neutral-100 gap-4 justify-items-center"> 
-    <header class="w-screen h-auto flex justify-center m-0 row-span-1 bg-neutral-100 ">
+<body> 
+    <div class="flex flex-col flex-wrap h-vh bg-neutral-100  justify-items-center"> 
+    <header class="w-screen h-32 flex justify-center m-0  bg-neutral-100 ">
         <div class="flex pl-6 pr-6 h-28 self-center border border-black border-solid rounded-xl bg-[#FAF9F6] ">
         <img class="w-24 h-24 mr-4 self-center" src="/slike/polica.png" alt="Polica s knjigama"> 
         <div class="flex items-center" ><h1 class="text-7xl font-sans mb-0">Biblioteka</h1></div>
         </div>
     </header>
-    <div class="w-screen  h-auto grid grid-rows-2 justify-center items-start gap-0 mt-40 pt-0 pb-0 row-span-2 bg-neutral-100">
-        <div class="flex flex-col justify-center items-center gap-4 pt-12">
+    <div class="w-screen h-96  flex flex-wrap justify-center pt-0 pb-0  bg-neutral-100">
+        <div class="flex flex-col justify-center items-center gap-4">
             <h1 class="flex text-3xl ">Izaberi 5 knjiga.</h1>
             <span><input type="text" id="unos" placeholder="Unesite naziv knjige."></span>
             <span class=" flex w-24"><button id="dugme" class="w-24 border-2 border-black">Pretraga!</button></span>
         </div>
         <div class="flex gap-4 align-top">
         <div id="ispis1" class="w-64 h-96 relative align-top grid grid-cols-1 m-0 border rounded-xl bg-neutral-200">
+            <div id="broj1" class="grid grid-cols-1 justify-self-center self-center justify-items-center margin-auto absolute">
+                <p class="text-2xl font-medium opacity-50">Knjiga</p>
+                <p class="text-9xl font-medium opacity-50">1</p>
+            </div>
+            <h1 class="text-xl justify-self-center self-top" id="naslov1"></h1>
+            <p id="autor1" class="justify-self-center self-top"></p>
+            <img id="slika1" src="" alt="Slika" class="w-180 h-248" style="display:none"><button id="x1" class="absolute top-2 right-4"></button>  </div>
+        <div id="ispis2" class="w-64 h-96 relative align-top  grid grid-cols-1 m-0 border rounded-xl bg-neutral-200">
+            <div id="broj2" class="grid grid-cols-1 justify-self-center self-center justify-items-center margin-auto absolute">
+                <p class="text-2xl font-medium opacity-50">Knjiga</p>
+                <p class="text-9xl font-medium opacity-50">2</p>
+            </div>            
             <h1 class="text-xl justify-self-center" id="naslov2"></h1>
             <p id="autor2" class="justify-self-center"></p>
-            <img id="slika2" src="" alt="Slika" style="display:none"><button id="x1" class="absolute top-2 right-4">&#9747;</button>  </div>
-        <div id="ispis2" class="w-64 h-96 relative align-top  grid grid-cols-1 m-0 border rounded-xl bg-neutral-200">
+            <img id="slika2" src="" alt="Slika" class="w-180 h-248 max-w-180 max-h-248" style="display:none"><button id="x2" class="absolute  top-2 right-4"></button> </div>
+        <div id="ispis3" class="w-64 h-96 relative align-top  grid grid-cols-1 m-0 border rounded-xl bg-neutral-200">
+            <div id="broj3" class="grid grid-cols-1 justify-self-center self-center justify-items-center margin-auto absolute">
+                <p class="text-2xl font-medium opacity-50">Knjiga</p>
+                <p class="text-9xl font-medium opacity-50">3</p>
+            </div>            
             <h1 class="text-xl justify-self-center" id="naslov3"></h1>
             <p id="autor3" class="justify-self-center"></p>
-            <img id="slika3" src="" alt="Slika" style="display:none"><button id="x2" class="absolute top-2 right-4">&#9747;</button> </div>
-        <div id="ispis3" class="w-64 h-96 relative align-top  grid grid-cols-1 m-0 border rounded-xl bg-neutral-200">
+            <img id="slika3" src="" alt="Slika" class="w-180 h-248" style="display:none"><button id="x3" class="absolute  top-2 right-4"></button> </div>
+        <div id="ispis4" class="w-64 h-96 relative align-top  grid grid-cols-1 m-0 border rounded-xl bg-neutral-200">
+            <div id="broj4" class="grid grid-cols-1 justify-self-center self-center justify-items-center margin-auto absolute">
+                <p class="text-2xl font-medium opacity-50">Knjiga</p>
+                <p class="text-9xl font-medium opacity-50">4</p>
+            </div>            
             <h1 class="text-xl justify-self-center" id="naslov4"></h1>
             <p id="autor4" class="justify-self-center"></p>
-            <img id="slika4" src="" alt="Slika" style="display:none"><button id="x3" class="absolute top-2 right-4">&#9747;</button> </div>
-        <div id="ispis4" class="w-64 h-96 relative align-top  grid grid-cols-1 m-0 border rounded-xl bg-neutral-200">
+            <img id="slika4" src="" alt="Slika" class="w-180 h-248" style="display:none"><button id="x4" class=" absolute  top-2 right-4"></button></div>
+        <div id="ispis5" class="w-64 h-96 relative align-top  grid grid-cols-1 m-0 border rounded-xl bg-neutral-200">
+            <div id="broj5" class="grid grid-cols-1 justify-self-center self-center justify-items-center margin-auto absolute">
+                <p class="text-2xl font-medium opacity-50">Knjiga</p>
+                <p class="text-9xl font-medium opacity-50">5</p>
+            </div>            
             <h1 class="text-xl justify-self-center" id="naslov5"></h1>
             <p id="autor5" class="justify-self-center"></p>
-            <img id="slika5" src="" alt="Slika" style="display:none"><button id="x4"  class="absolute top-2 right-4">&#9747;</button> </div>
-        <div id="ispis5" class="w-64 h-96 relative align-top  grid grid-cols-1 m-0 border rounded-xl bg-neutral-200">
-            <h1 class="text-xl justify-self-center" id="naslov6"></h1>
-            <p id="autor6" class="justify-self-center"></p>
-            <img id="slika6" src="" alt="Slika" style="display:none"><button id="x5" class="absolute top-2 right-4">&#9747;</button> </div>
+            <img id="slika5" src="" alt="Slika" class="w-180 h-248" style="display:none"><button id="x5" class="absolute  top-2 right-4"></button> </div>
         </div>
         </div>
 
-    <footer class="w-screen h-auto flex justify-between content-center self-end m-0 p-6 row-span-1 bg-neutral-400 border-4 border-neutral-500">
+    <footer class="w-screen h-32 flex justify-between content-center self-end m-0 p-6  bg-neutral-400 border-4 border-neutral-500">
         <p class="self-center">Made by me </p><button href="/">Back to top</button>
     </footer>
+    </div>
     @vite('resources/js/api.js')
 </body> 
 </html>
